0666c1605b21c9da1a9a656ac9f288c9592100a0
asd881018 <asd881018@yahoo.com.tw>
2023-11-07 23:09:06 -0800

    Assn3 Task1 Done

--- assignment3/queues/one_lock_queue.h
+++ assignment3/queues/one_lock_queue.h
@@ -1,14 +1,21 @@
 #include "../common/allocator.h"
+#include <mutex>
 
 template <class T>
 class Node
 {
+public:
+    T value;
+    Node<T>* next;
 };
 
 template <class T>
 class OneLockQueue
 {
+    Node<T>* q_head;
+    Node<T>* q_tail;
     CustomAllocator my_allocator_;
+    std::mutex mutex_lock; // Mutex for synchronization
 public:
     OneLockQueue() : my_allocator_()
     {
@@ -18,19 +25,56 @@ public:
     void initQueue(long t_my_allocator_size){
         std::cout << "Using Allocator\n";
         my_allocator_.initialize(t_my_allocator_size, sizeof(Node<T>));
+
         // Initialize the queue head or tail here
-        Node<T>* newNode = (Node<T>*)my_allocator_.newNode();
-        my_allocator_.freeNode(newNode);
+        Node<T>* new_node = (Node<T>*)my_allocator_.newNode();
+        if (!new_node){
+            std::cout << "Error in allocating memory\n";
+            exit(EXIT_FAILURE);
+        }
+        
+        new_node->next = nullptr;
+        q_head = new_node;
+        q_tail = new_node;
+        my_allocator_.freeNode(new_node);
     }
 
     void enqueue(T value)
     {
+        mutex_lock.lock();
+        Node<T>* node = (Node<T>*)my_allocator_.newNode();
+        node->value = value;
+        node->next = nullptr;
+
+        q_tail->next = node;
+        q_tail = node;
+        mutex_lock.unlock();
     }
 
     bool dequeue(T *value)
-    {
-        bool ret_value = false;
-        return ret_value;
+    {   
+        mutex_lock.lock();
+
+        Node<T>* node = q_head;
+        Node<T>* new_head = q_head->next;
+
+        if (new_head == NULL) {
+            // Queue is empty
+            mutex_lock.unlock();
+            return false;
+        }
+
+        *value = new_head->value;
+        q_head = new_head;
+        
+        // If the queue is now empty after dequeue
+        if (q_head == NULL) { 
+            q_tail = NULL;
+        }
+        mutex_lock.unlock();
+        my_allocator_.freeNode(node);
+        
+        return true;
     }
 
     void cleanup()
